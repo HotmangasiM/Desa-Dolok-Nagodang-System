def7609f934a6c4ceeabf1e4eba0f939f1de576c
RafelliSimangunsong <rafellisimangunsong@gmail.com>
2026-04-03 22:33:33 +0700

    Test commit

--- desa-dolok-nagodang-app/routes/web.php
+++ desa-dolok-nagodang-app/routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 // Route::apiResource('citizens', 'App\Http\Controllers\Api\CitizenController');
+Route::get('/test', function () {
+    return 'Laravel is working!';
+});
