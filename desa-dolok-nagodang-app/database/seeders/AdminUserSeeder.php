6e12c2972ecca8495d8258f0a90df4f95dfddab9
HotmangasiM <hotmangasi25@gmail.com>
2026-04-18 23:07:08 +0700

    update 18-04-2026 add authentication for admin page

---
+++ desa-dolok-nagodang-app/database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@desa.test'],
+            [
+                'name' => 'Administrator Desa',
+                'password' => 'admin12345',
+                'role' => 'admin',
+                'is_active' => true,
+            ]
+        );
+    }
+}
