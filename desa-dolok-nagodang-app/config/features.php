da2453b553938950231a063c275b9532e3b78664
HotmangasiM <hotmangasi25@gmail.com>
2026-06-09 13:37:23 +0700

    Update 09-06-2026: update seeder

--- desa-dolok-nagodang-app/config/features.php
+++ desa-dolok-nagodang-app/config/features.php
@@ -2,4 +2,5 @@
 
 return [
     'public_letter_submission' => env('FEATURE_PUBLIC_SUBMISSION', false),
+    'registration' => env('FEATURE_REGISTRATION', in_array(env('APP_ENV', 'production'), ['local', 'testing'], true)),
 ];
